e10261499988b5f26d70555f182211120bb775e5
zeon <scplay@gmail.com>
2017-07-15 11:45:17 +0800

    Initial Commit

---
+++ hello_world_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello_world_c
+//
+//  Created by Jenny on 2017/7/15.
+//  Copyright © 2017年 Jenny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
